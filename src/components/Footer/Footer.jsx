10fe79680d4c590dc74b57b3171ec2890f86ca39
prabaharan-saminathan <139528188+prabaharan-saminathan@users.noreply.github.com>
2024-05-17 22:33:38 +0530

    main

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,51 @@
+import React from "react"
+import "./style.css"
+import { Col, Container, Row } from "react-bootstrap"
+
+const Footer = () => {
+  return (
+    <footer>
+        <Container>
+          <Row className="footer-row">
+            <Col md={3} sm={5} className='box'>
+              <div className="logo">
+                  <ion-icon name="bag"></ion-icon>
+                  <h1>SPIKKER-SHOP</h1>
+              </div>
+              <p>"I'm in the mood for some retail therapy. Let's hit the shops!","Have you seen the latest fashion trends? I need to update my wardrobe".</p>
+            </Col>
+            <Col md={3} sm={5} className='box'>
+              <h2>About Us</h2>
+              <ul>
+                <li>Careers</li>
+                <li>Our Stores</li>
+                <li>Our Cares</li>
+                <li>Terms & Conditions</li>
+                <li>Privacy Policy</li>
+              </ul>
+            </Col>
+            <Col md={3} sm={5} className='box'>
+              <h2>Customer Care</h2>
+              <ul>
+                <li>Help Center </li>
+                <li>How to Buy </li>
+                <li>Track Your Order </li>
+                <li>Corporate & Bulk Purchasing </li>
+                <li>Returns & Refunds </li>
+              </ul>
+            </Col>
+            <Col md={3} sm={5} className='box'>
+              <h2>Contact Us</h2>
+              <ul>
+                <li>70c,Anna Nagar,Chennai-600001 </li>
+                <li>Email: praba.help@gmail.com</li>
+                <li>Phone: +9178*******7</li>
+              </ul>
+            </Col>
+          </Row>
+        </Container>
+    </footer>
+  )
+}
+
+export default Footer
